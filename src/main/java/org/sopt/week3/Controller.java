522c84f1f28809ac8aeb8aab10ae45ba93c25d58
seoyeonjiin <duswls214@khu.ac.kr>
2024-10-27 23:46:11 +0900

    Feat: 3차 세미나 내용 정리

---
+++ src/main/java/org/sopt/week3/Controller.java
@@ -0,0 +1,31 @@
+package org.sopt.week3;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Controller {
+    private final SoptMemberRepository soptMemberRepository;
+
+    public Controller(SoptMemberRepository soptMemberRepository){
+        this.soptMemberRepository = soptMemberRepository;
+    }
+    @PostMapping("/member")
+    void postMember() {
+        soptMemberRepository.save(
+                new SoptMemberEntity("yon", 12)
+        );
+    }
+
+    @GetMapping("/members")
+    ResponseEntity<String> getMembers() {
+        List<SoptMemberEntity> all = soptMemberRepository.findAll();
+
+        return ResponseEntity.ok(all.stream().findFirst().get().toString());
+    }
+
+}
